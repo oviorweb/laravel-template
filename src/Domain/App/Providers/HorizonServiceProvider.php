5a235ff71c3b54a817fe0888dc0da35a1b5b6a36
Dany Gagnon <danygagnon@Danys-MacBook-Air.local>
2022-09-30 17:20:52 -0400

    feat: add laravel horizon

---
+++ src/Domain/App/Providers/HorizonServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Domain\App\Providers;
+
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+         Horizon::routeMailNotificationsTo('ddanygagnon@gmail.com');
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Horizon::auth(fn ($request) => true);
+    }
+}
